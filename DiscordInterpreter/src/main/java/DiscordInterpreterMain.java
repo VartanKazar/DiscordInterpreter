6c78ee1663e12b01d054a333e3fd8b76024cbf4a
VartanKazar <57660862+VartanKazar@users.noreply.github.com>
2020-02-01 15:10:14 -0800

    project setup

---
+++ DiscordInterpreter/src/main/java/DiscordInterpreterMain.java
@@ -0,0 +1,9 @@
+import net.dv8tion.jda.api.*;
+
+public class DiscordInterpreterMain {
+    public static void main(String[] arguments) throws Exception
+    {
+        JDA api = new JDABuilder("NjczMDc5NTQ1MjYzNTU0NTkx.XjU0bw.q2IPjrc0KzqAPFnuvPRiiNmqYpE").build();
+    }
+
+}
