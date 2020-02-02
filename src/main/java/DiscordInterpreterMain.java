fa2b4ca10a8f6e8ced60aae38a96784aaf45fb2f
VartanKaze <vart_kaze93@yahoo.com>
2020-02-01 19:17:08 -0800

    added git ignore and experimental code in main

--- src/main/java/DiscordInterpreterMain.java
+++ src/main/java/DiscordInterpreterMain.java
@@ -1,9 +1,30 @@
 import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.VoiceChannel;
+
+import java.util.List;
 
 public class DiscordInterpreterMain {
     public static void main(String[] arguments) throws Exception
     {
-        JDA api = new JDABuilder("NjczMDc5NTQ1MjYzNTU0NTkx.XjU0bw.q2IPjrc0KzqAPFnuvPRiiNmqYpE").build();
+        /*
+        Setting up the JDA builder to accept a discord bot token.
+
+
+         */
+        JDA api = new JDABuilder("NjczMzMxNTYzMjE5NTE3NDQw.XjYfAg.sYo2Cwho85ie1VBbxGJGksc_x3M").build();
+
+        List<VoiceChannel> channels = api.getVoiceChannels();
+
+
+        VoiceChannel voiceChannel = channels.get(0);
+
+        System.out.print(voiceChannel.getName());
+
+
+
+
+        //System.out.print(channels);
     }
 
 }
