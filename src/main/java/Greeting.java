984d84b10a44d73846beb8fbb18b95008598cbda
tracy <tracytsmart@gmail.com>
2020-02-01 20:08:06 -0800

    Added the Bot greeting functionality

---
+++ src/main/java/Greeting.java
@@ -0,0 +1,13 @@
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Greeting extends ListenerAdapter {
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event){
+        String messageSent = event.getMessage().getContentRaw();
+
+        if(messageSent.equalsIgnoreCase("hello")) {
+            event.getChannel().sendMessage("Hi welcome!").queue();
+
+        }
+    }
+}
