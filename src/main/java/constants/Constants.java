f37e1c76d3be3624be53bf43bcf8730760aac926
tracy <tracytsmart@gmail.com>
2020-02-02 09:15:18 -0800

    Added the Bot greeting functionality. Bot is now able to join and leave voice channels

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -2,5 +2,5 @@ package constants;
 
 public class Constants {
 
-    public static final String TOKEN = "NjczMDc5NTQ1MjYzNTU0NTkx.XjYCCg.x30oKvLi2llPUhJSXbRrSXlJeDg";
+    public static final String TOKEN = "NjczMDc5NTQ1MjYzNTU0NTkx.XjZ4ew.96uwsVwL17MLAeHupjKi1R70Jz4";
 }
